39abd38ca831f939838e18564b303113afcbf759
dhoniaridho <dhoniaridho@gmail.com>
2024-11-15 13:28:33 +0700

    fix: add suspense handler

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Suspense } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NuqsAdapter>{children}</NuqsAdapter>
+      <NuqsAdapter>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      </NuqsAdapter>
     </NextUIProvider>
   );
 }
