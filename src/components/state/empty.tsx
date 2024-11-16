93c5842316362cd23f4ecaca68946c02233110e5
dhoniaridho <dhoniaridho@gmail.com>
2024-11-16 20:42:37 +0700

    fix: ui empty state

--- src/components/state/empty.tsx
+++ src/components/state/empty.tsx
@@ -5,7 +5,11 @@ import Lootie from "lottie-react";
 export default function EmptyState() {
   return (
     <div>
-      <Lootie animationData={Empty} />
+      <Lootie className="max-w-xs mx-auto" animationData={Empty} />
+      <div className="mt-3 text-slate-600">
+        <div className="text-2xl font-semibold text-primary">Data Tidak Ditemukan</div>
+        <div className="text-sm">Silahkan tambahkan data di menu tambah</div>
+      </div>
     </div>
   );
 }
