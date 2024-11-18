5f3eb2414ab9dedd6f299491fbcaf2d0f59776da
dhoniaridho <dhoniaridho@gmail.com>
2024-11-18 14:18:41 +0700

    fix: add missing menu

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -4,14 +4,17 @@ import { Avatar, Button, Card, CardBody, cn } from "@nextui-org/react";
 import Link from "next/link";
 import {
   HiArrowRightOnRectangle,
+  HiCircleStack,
+  HiInbox,
   HiOutlineHome,
+  HiUserPlus,
   HiUsers,
 } from "react-icons/hi2";
 import { AnimatePresence, motion } from "framer-motion";
 import { useGetProfile } from "./_services/profile";
 import { signOut } from "./sign-out/_actions/sign-out";
 import { usePathname } from "next/navigation";
-import { HiLocationMarker, HiMenuAlt2, HiMenuAlt4, HiX } from "react-icons/hi";
+import { HiInboxIn, HiLocationMarker, HiMenuAlt2, HiMenuAlt4, HiX } from "react-icons/hi";
 import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
