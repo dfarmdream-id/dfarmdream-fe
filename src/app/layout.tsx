2efc040f102c155ae9da1f563075d1013719288a
dhoniaridho <dhoniaridho@gmail.com>
2024-11-24 22:52:05 +0700

    fix: disable scale mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "DFarm Dream",
   description: "DFarm Dream",
+};
+
+export const viewport: Viewport = {
+  userScalable: false,
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  width: "device-width",
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
@@ -22,7 +31,10 @@ export default function RootLayout({
           sizes="96x96"
         />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
