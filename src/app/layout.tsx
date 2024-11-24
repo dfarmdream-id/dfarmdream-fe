25da2abd830e1b9a24dfb5b8ba115f754e60989d
dhoniaridho <dhoniaridho@gmail.com>
2024-11-24 22:25:36 +0700

    fix: disable scale mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
           sizes="96x96"
         />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
