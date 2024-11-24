ae5d441731f80b83cc183f448cd00a2a69ccad9f
dhoniaridho <dhoniaridho@gmail.com>
2024-11-24 23:42:24 +0700

    fix:  webmanifest redirected

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|apple-touch-icon|site.webmanifest).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|apple-touch-icon|manifest.webmanifest).*)"],
 };
 
 // This function can be marked `async` if using `await` inside
